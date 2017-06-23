8cd14b601288cf63dfec0f813f7e5fe2aa7ad186
jbarget <dear.justen@gmail.com>
2017-06-23 17:24:54 +0100

    adding flush check and pseudo code for straight check

--- lib/dojo/hand.ex
+++ lib/dojo/hand.ex
@@ -5,5 +5,27 @@ defmodule Dojo.Hand do
     evaluate(cards)
   end
 
-  defp evaluate(_), do: :high_card
+  defp evaluate(cards) do
+
+    cond do
+      is_straight(cards) && is_same_suit(cards) ->
+        :straight_flush
+      is_straight(cards) ->
+        :straight
+      is_same_suit(cards) ->
+        :flush
+      true ->
+        :high_card
+    end
+  end
+
+  defp is_straight(cards) do
+    # sort cards by rank
+    # Map (reduce) through checking rank of n == rank of (n -1) +1 
+  end
+
+  defp is_same_suit(cards) do
+    Enum.all?(tail, fn(%{rank: _, suit: suit}) -> suit == initialSuit end)
+  end
+
 end
